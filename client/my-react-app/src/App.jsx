07505e295832ead8682fc7352585a867a07723f8
Gayanand <gayanand.patel@gmail.com>
2026-01-22 04:01:10 +0530

    setup main routing -app.jsx

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// Import Pages
+import Login from './pages/Login';
+import Register from './pages/Register';
+import PrivateRoute from './components/PrivateRoute';
+
+// Dummy Dashboard Component
+const Dashboard = () => <h1>Welcome to Dashboard</h1>;
 
 function App() {
-
   return (
-    <>
-      <h1>Hello REact!!!</h1>
-    </>
-  )
+    <Router>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        {/* Default redirect to login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Protected Routes */}
+        <Route 
+          path="/dashboard" 
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          } 
+        />
+        <Route 
+          path="/tasks" 
+          element={
+            <PrivateRoute>
+              <Dashboard /> {/* Re-using Dashboard for now */}
+            </PrivateRoute>
+          } 
+        />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;

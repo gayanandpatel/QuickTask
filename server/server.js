3928057ca4e9a0a91cea2c890ca1c99fd141c89b
Gayanand <gayanand.patel@gmail.com>
2026-01-22 01:13:50 +0530

    create entry point -  server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express, { json } from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db';
+
+// Load environment variables
+config();
+
+// Connect to Database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors()); // Allows frontend to communicate with backend
+app.use(json()); // Allows the backend to understand JSON data
+
+// A simple test route to ensure server is working
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start the Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
